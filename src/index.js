7ffc703bb978e26284ea39887d8cc9a38082031c
LukasHe0908-test <88268059+LukasHe0908-test@users.noreply.github.com>
2024-02-08 22:07:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const dotenv = require('dotenv');
 
 const CoreConfigHandler = require('./utils/coreConfigHandler.js');
 let err404 = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Page not found</title><style>*{margin:0;padding:0;font-family:sans-serif}body,html{width:100%;height:100%;overflow:auto;color:#000;background-color:#fff;box-sizing:border-box}.container{margin-left:auto;margin-right:auto;--padding:18px;padding-left:var(--padding);padding-right:var(--padding);width:calc(100% - var(--padding) * 2);margin-top:80px;border-radius:8px;transition:all ease .2s,background-color 0s,color 0s}@media (min-width:980px){.container{max-width:978px}}@media (min-width:1200px){.container{max-width:1128px}}@media (max-height:500px){.container{margin-top:18px}}#backHome{display:inline-flex;align-items:center;color:#fff;background-color:#333;border-radius:16px;margin:16px 0 0;padding:16px 32px;transition:all ease .2s,background-color 0s,color 0s;user-select:none}#backHome:hover{transition:all ease .2s;background-color:#2a2a2a}#backHome:active{transition:all ease .2s;background-color:#2a2a2a;transform:scale(.95)}#backHome svg{fill:#fff}.col-2{color:#222}.headline-1{font-size:3em;font-weight:600;margin-bottom:24px}.headline-5{font-size:1em;font-weight:600;margin-bottom:0}</style></head><body><div class="container"><div><h1 class="headline-1 col-2">Page not found</h1><h5 class="headline-5 col-2">Sorry, we can't find the page you are looking for in this download server.</h5></div><div id="backHome">Back Home</div></div></body><script>document.querySelector('#backHome').addEventListener('click', () => { location.pathname = "/" });</script></html>`;
+let handlepage = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width,initial-scale=1" /><title>Handle Page</title><style>*{margin:0;padding:0;font-family:sans-serif;box-sizing:border-box;scrollbar-width:thin}body,html{width:100%;height:100%;overflow:hidden;color:#000;background-color:#fff}body{display:flex;flex-direction:row}@media (max-width:980px){body{flex-direction:column}}.container{display:inline-block;--padding:18px;padding-left:var(--padding);border-radius:8px;transition:all ease .2s,background-color 0s,color 0s}.btn{display:inline-flex;align-items:center;color:#fff;background-color:#333;border-radius:12px;padding:8px 16px;margin-right:4px;margin-bottom:4px;transition:all ease .2s,background-color 0s,color 0s;user-select:none}.btn:hover{transition:all ease .2s;background-color:#2a2a2a}.btn:active{transition:all ease .2s;background-color:#2a2a2a;transform:scale(.95)}.btn svg{fill:#fff}.col-2{color:#222}.headline-1{font-size:3em;font-weight:600;margin-bottom:12px}.headline-5{font-size:1em;font-weight:600;margin-bottom:0}iframe{border:#181818 2px solid;width:100%;height:100%;border-radius:12px}.container2{display:inline-block;width:100%;height:100%;padding:8px}</style></head><body><div class="container"><h1 class="headline-1 col-2">Handle Page</h1><div><h5 class="headline-5 col-2">Update</h5><div class="btn">Restart</div><div class="btn">Core</div><div class="btn">Argo</div><div class="btn">All</div><br /><br /><h5 class="headline-5 col-2">Version</h5><div class="btn">Get</div><div class="btn">Clear</div></div></div><div class="container2"><iframe src="about:blank" frameborder="0"></iframe></div></body><script>let iframe=document.querySelector("iframe");document.querySelectorAll(".btn").forEach(element=>{element.addEventListener("click",()=>{switch(element.innerHTML){case"Restart":iframe.src=location.href+"/update";break;case"Core":iframe.src=location.href+"/update?core";break;case"Argo":iframe.src=location.href+"/update?argo";break;case"All":iframe.src=location.href+"/update?core&argo";break;case"Get":iframe.src=location.href+"/version";break;default:iframe.src="about:blank";break}})});</script></html>`;
 
 dotenv.config();
 const app = express();
@@ -27,9 +28,10 @@ const config = (() => {
       config_json = {};
     }
   }
-  let part_warp;
+  let part_warp = {};
   if (config_json['warp']) {
     part_warp = {
+      ...part_warp,
       warp_secretKey: config_json['warp']['key'] || '',
       warp_ipv6: config_json['warp']['ipv6'] || '',
       warp_endpoint:
@@ -38,21 +40,24 @@ const config = (() => {
       add_ipv6: config_json['warp']['add6'] || false,
     };
   }
-  let part_argo;
+  let part_argo = {
+    argo_path:
+      config_json['argo_path'] ||
+      (os.platform() == 'win32' ? './cloudflared.exe' : './cloudflared'),
+  };
   if (config_json['argo']) {
     part_argo = {
-      argo_path:
-        config_json['argo_path'] ||
-        (os.platform() == 'win32' ? './cloudflared.exe' : './cloudflared'),
+      ...part_argo,
       use_argo: config_json['argo']['use'] || false,
       argo_protocol: config_json['argo']['protocol'] || '',
       argo_region: config_json['argo']['region'] || '',
       argo_access_token: config_json['argo']['token'] || '',
     };
   }
-  let part_tls;
+  let part_tls = {};
   if (config_json['tls']) {
     part_tls = {
+      ...part_tls,
       use_tls: config_json['tls']['use'] || false,
       // please use base64 encode
       tls_key:
@@ -71,6 +76,10 @@ const config = (() => {
     protocol: config_json['protocol'] || 'dmxlc3M=',
     uuid: config_json['uuid'] || guid(),
     path: config_json['path'] || '/api',
+    display_web_entry: config_json['display_web_entry'] || false,
+    web_process: config_json['web_process'] || false,
+    web_process_path: config_json['web_process_path'] || '',
+    web_process_debug: config_json['web_process_debug'] || false,
     // tls
     ...part_tls,
     // warp
@@ -88,14 +97,119 @@ function guid() {
   });
 }
 
+let pid_core = NaN,
+  pid_argo = NaN;
+
 app.get('/generate_204', (req, res) => {
   res.status(204).send('');
 });
-app.get('/debug' + config.path, (req, res) => {
-  res
-    .setHeader('Content-Type', 'text/plain')
-    .send(cp.execSync(`ps aux|sort -rn -k +4|head -50`).toString());
+app.get(config.path + config.web_process_path, (req, res, next) => {
+  if (config.display_web_entry) {
+    res.send(handlepage);
+  } else {
+    next();
+  }
 });
+app.get(config.path + config.web_process_path + '/debug', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.setHeader('Transfer-Encoding', 'chunked');
+  if (!config.web_process_debug) {
+    res.end('web_process_debug off');
+    return;
+  }
+  res.end(cp.execSync(`ps aux|sort -rn -k +4|head -50`).toString());
+});
+app.get(config.path + config.web_process_path + '/update', async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.setHeader('Transfer-Encoding', 'chunked');
+  if (!config.web_process) {
+    res.end('web_process off');
+    return;
+  }
+  res.write('---- Start');
+  if (!isNaN(pid_argo)) process.kill(pid_argo);
+  if (!isNaN(pid_core)) process.kill(pid_core);
+  pid_core = NaN;
+  pid_argo = NaN;
+  if (typeof req.query['argo'] == 'string') {
+    try {
+      const foo = await download_argo();
+      if (foo) {
+        res.write('\n' + 'argo下载成功' + '\n    ' + foo);
+      } else {
+        res.write('\n' + 'argo下载失败' + '\n    ' + foo);
+      }
+    } catch (err) {
+      res.write('\n' + 'argo下载失败' + '\n    ' + err);
+    }
+  }
+  if (typeof req.query['core'] == 'string') {
+    try {
+      const foo = await download_core();
+      if (foo) {
+        res.write('\n' + 'core载成功' + '\n    ' + foo);
+      } else {
+        res.write('\n' + 'core下载失败' + '\n    ' + foo);
+      }
+    } catch (err) {
+      res.write('\n' + 'core下载失败' + '\n    ' + err);
+    }
+  }
+
+  start(true);
+  res.end('\n' + '---- Done');
+});
+app.get(
+  config.path + config.web_process_path + '/version',
+  async (req, res) => {
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.setHeader('Transfer-Encoding', 'chunked');
+    if (!config.web_process) {
+      res.end('web_process off');
+      return;
+    }
+    res.write(`Node version:`);
+    let object = process.versions;
+    for (const key in object) {
+      if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        res.write(`\n    ${key}: ${element}`);
+      }
+    }
+    res.write(`\n\nCore version:`);
+    const core_version = await (_ => {
+      return new Promise(async resolve => {
+        let args = ['--version'];
+        let processC = cp.spawn(config.core_path, args);
+        let pData = '';
+        processC.stdout.on('data', data => {
+          pData += data.toString();
+        });
+        processC.on('close', () => {
+          resolve(pData);
+        });
+      });
+    })();
+    res.write(`\n    ${core_version}`);
+    res.write(`\n\nArgo version:`);
+    const argo_version = await (_ => {
+      return new Promise(async resolve => {
+        let args = ['--version'];
+        let processC = cp.spawn(config.argo_path, args);
+        let pData = '';
+        processC.stdout.on('data', data => {
+          pData += data.toString();
+        });
+        processC.on('close', () => {
+          resolve(pData);
+        });
+      });
+    })();
+    res.write(`\n    ${argo_version}`);
+
+    res.end(null);
+  }
+);
 
 const wsProxy = httpProxyMiddleware.createProxyMiddleware({
   target: `http://127.0.0.1:${config.middle_port}/`,
@@ -103,7 +217,6 @@ const wsProxy = httpProxyMiddleware.createProxyMiddleware({
   ws: true,
   logLevel: 'silent', // 禁用所有日志输出
 });
-app.use(config.path, wsProxy);
 // 404 page
 app.use((req, res, next) => {
   res.status(404).send(err404);
@@ -196,6 +309,7 @@ async function start_core() {
           // protocol: 'freedom',
           protocol: 'wireguard',
           settings: {
+            kernelMode: false,
             secretKey: config.warp_secretKey, // 粘贴你的 "private_key" 值
             address: [
               '172.16.0.2/32',
@@ -271,15 +385,15 @@ async function start_core() {
       // console.log(data.toString().trim());
       if (/[0-9/]* [0-9:]* \[Warning\] core: .* started/.test(data)) {
         console.log(`----------
-  [Config]
-  path: ${config.path}
-  uuid: ${config.uuid}
-  ----------`);
-        resolve('[初始化] core启动成功');
+[Config]
+path: ${config.path}
+uuid: ${config.uuid}
+----------`);
+        resolve([true, processC.pid]);
       }
     });
     processC.on('error', err => {
-      resolve('[初始化] core启动错误：' + err);
+      resolve([false, err]);
     });
   });
 }
@@ -396,10 +510,10 @@ async function start_argo() {
       } else {
         // console.log(data.toString().trim());
       }
-      resolve('[初始化] argo启动成功');
+      resolve([true, processC.pid]);
     });
     processC.on('error', err => {
-      resolve('[初始化] argo启动错误：' + err);
+      resolve([false, err]);
     });
   });
 }
@@ -421,17 +535,35 @@ function listen_port() {
   } else {
     serverProxy = http.createServer(app);
   }
-  serverProxy.listen(config.port, () => {
-    console.log('[软件]', `Listening on port ${config.port}`);
+  const try_connect = serverProxy => {
+    serverProxy.listen(config.port, () => {
+      console.log('[软件]', `Listening on port ${config.port}`);
+    });
+  };
+  try_connect(serverProxy);
+  serverProxy.on('error', e => {
+    if (e.code === 'EADDRINUSE') {
+      console.error('Address in use, retrying...');
+      setTimeout(() => {
+        serverProxy.close();
+        try_connect(serverProxy);
+      }, 1000);
+    }
   });
-  // serverProxy.on('upgrade', wsProxy.upgrade);
+  serverProxy.on('upgrade', (request, socket, head) => {
+    if (request.url.split('?')[0] === config.path) {
+      wsProxy.upgrade(request, socket, head);
+    } else {
+      socket.destroy();
+    }
+  });
 }
 
 (async _ => {
   // console.log(await start_argo());
 })();
 start();
-async function start() {
+async function start(no_listen_port = false) {
   if (config.use_argo) {
     if (!fs.existsSync(path.resolve(process.cwd(), config.argo_path))) {
       const foo = await download_argo();
@@ -443,7 +575,13 @@ async function start() {
     } else {
       console.log('[初始化]', 'argo已存在');
     }
-    console.log(await start_argo());
+    const start_return = await start_argo();
+    if (start_return[0]) {
+      pid_argo = start_return[1];
+      console.log('[初始化]', 'argo启动成功');
+    } else {
+      console.log('[初始化]', 'argo启动失败：', start_return[1]);
+    }
   }
 
   if (!fs.existsSync(path.resolve(process.cwd(), config.core_path))) {
@@ -456,8 +594,15 @@ async function start() {
   } else {
     console.log('[初始化]', 'core已存在');
   }
-  console.log(await start_core());
-  listen_port();
+  const start_return = await start_core();
+  if (start_return[0]) {
+    pid_core = start_return[1];
+    console.log('[初始化]', 'core启动成功');
+  } else {
+    console.log('[初始化]', 'core启动失败：', start_return[1]);
+  }
+
+  if (!no_listen_port) listen_port();
 }
 
 keepalive();
